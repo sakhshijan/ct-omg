8298c7fa2c40a184a9f899304b7623a46e65b8d1
Saeed1112 <nullee.sa@gmail.com>
2023-07-15 15:14:28 +0330

    .

--- public/script/main.js
+++ public/script/main.js
@@ -1,5 +1,7 @@
+const DATE_PICKER_SHOW_ITEMS = 5;
 $(document).ready(function () {
   registerChangeDateAndTime();
+  datePickerComponent();
 });
 
 function registerChangeDateAndTime() {
@@ -13,4 +15,145 @@ function registerChangeDateAndTime() {
     const _this = $(this);
     dialogBox.toggleClass("show");
   });
+}
+
+function datePickerComponent() {
+  // moment.from('1402/12/04', 'fa', 'YYYY/MM/DD').jDaysInMonth()
+  (function dateInput() {
+    const [year, month, day] = moment()
+      .locale("fa")
+      .format("YYYY/M/D")
+      .split("/")
+      .map(Number);
+    updateValue({ day, year, month });
+    inputDays(31, day);
+    inputMonths(month);
+    inputYears(year);
+  })();
+
+  function inputDays(totalDays, init) {
+    const days = $(".date-input-days");
+    const config = {
+      direction: "vertical",
+      loop: true,
+      slidesPerView: DATE_PICKER_SHOW_ITEMS,
+      grabCursor: true,
+      centeredSlides: true,
+      on: {
+        afterInit: function () {
+          if (init) this.slideTo(DATE_PICKER_SHOW_ITEMS + init - 1);
+        },
+        activeIndexChange: function () {
+          setTimeout(() => {
+            const index = days
+              .find(".swiper-slide-active")
+              .attr("data-swiper-slide-index");
+            updateValue({ day: +index + 1 });
+          }, 0);
+        },
+      },
+    };
+    Array.from(new Array(totalDays)).forEach((_, index) =>
+      days.append(`<li
+                  class="swiper-slide date-input flex items-center justify-center text-2xl font-semibold"
+                  data-value="${index + 1}"
+                >
+                  ${index + 1}
+                </li>`)
+    );
+    const swiper = new Swiper(".days", config);
+  }
+
+  function inputMonths(init) {
+    const _months = [
+      "فروردین",
+      "اردیبهشت",
+      "خرداد",
+      "تیر",
+      "مرداد",
+      "شهریور",
+      "مهر",
+      "آبان",
+      "آذر",
+      "دی",
+      "بهمن",
+      "اسفند",
+    ];
+    const months = $(".date-input-months");
+    const config = {
+      direction: "vertical",
+      loop: true,
+      slidesPerView: DATE_PICKER_SHOW_ITEMS,
+      grabCursor: true,
+      centeredSlides: true,
+      on: {
+        afterInit: function () {
+          if (init) this.slideTo(DATE_PICKER_SHOW_ITEMS + init - 1);
+        },
+        activeIndexChange: function () {
+          setTimeout(() => {
+            const index = months
+              .find(".swiper-slide-active")
+              .attr("data-swiper-slide-index");
+            updateValue({ month: +index + 1 });
+          }, 0);
+        },
+      },
+    };
+    _months.map((name, index) => {
+      months.append(
+        `<li class="swiper-slide date-input flex items-center justify-center text-2xl font-semibold" data-value="${
+          index + 1
+        }">${name}</li>`
+      );
+    });
+
+    const swiper = new Swiper(".months", config);
+  }
+
+  function inputYears(init) {
+    const show = 8;
+    const from = init - 3;
+    const days = $(".date-input-years");
+    const years = Array.from(new Array(show)).map(
+      (_, index) => from + index + 1
+    );
+    const config = {
+      direction: "vertical",
+      slidesPerView: DATE_PICKER_SHOW_ITEMS,
+      grabCursor: true,
+      centeredSlides: true,
+      on: {
+        afterInit: function () {
+          if (init) this.slideTo(DATE_PICKER_SHOW_ITEMS + (from - init));
+        },
+        activeIndexChange: function () {
+          if (years[this.activeIndex] < init)
+            this.slideTo(DATE_PICKER_SHOW_ITEMS + (from - init));
+          updateValue({ year: years[this.activeIndex] });
+        },
+      },
+    };
+    years.forEach((v) =>
+      days.append(
+        `<li class="swiper-slide date-input flex items-center justify-center text-2xl font-semibold">${v}</li>`
+      )
+    );
+    const swiper = new Swiper(".years", config);
+  }
+
+  function updateValue(
+    { day, month, year } = { day: undefined, month: undefined, year: undefined }
+  ) {
+    const input = $("[data-date-input]");
+    const oldValue = input.val().split("/");
+    if (oldValue.length !== 3) {
+      input.val(`${year}/${month}/${day}`);
+      return;
+    }
+    const value = [year, month, day].map((v, index) =>
+      v ? v : oldValue[index]
+    );
+    input.val(value.join("/"));
+  }
 }
