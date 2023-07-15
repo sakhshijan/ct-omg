c9ab025960a6b6bb8bc139207fdcc846e04467f1
Saeed1112 <nullee.sa@gmail.com>
2023-07-15 18:04:08 +0330

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    compressHTML: true,
     output: 'static',
     vite: {
         build: {
             cssCodeSplit: false,
-            rollupOptions: {
-                output: {
-                    entryFileNames: 'scripts/[name].js',
-                    chunkFileNames: 'chunks/[name].js',
-                    assetFileNames: 'assets/[name][extname]'
-                }
-            }
+            // rollupOptions: {
+            //     output: {
+            //         entryFileNames: 'scripts/[name].js',
+            //         chunkFileNames: 'chunks/[name].js',
+            //         assetFileNames: 'assets/[name][extname]'
+            //     }
+            // }
         }
     },
-  integrations: [tailwind()]
+    integrations: [tailwind()]
 });
